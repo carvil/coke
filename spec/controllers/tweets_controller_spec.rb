a8ad6f958fa9ec6b1df62867208192dfe6d563ea
Carlos Vilhena <carlosvilhena@gmail.com>
2012-12-09 18:14:19 +0000

    make sure the index action in tweets returns the correct sorted data

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -7,6 +7,21 @@ describe TweetsController do
       get 'index'
       response.should be_success
     end
+
+    context "when requesting json" do
+
+      it "should return a list of tweets in json ordered by sentiment" do
+        VCR.use_cassette('success_controller_testing', :record => :new_episodes) do
+          Tweet.load
+          get 'index', format: :json
+          JSON.parse(response.body).should eq([
+            {"_id"=>3, "count"=>1, "created_at"=>"2012-09-27T16:11:15Z", "followers"=>24, "id"=>3, "message"=>"Coke is it!", "sentiment"=>"1.0", "updated_at"=>"2012-09-27T16:11:15Z", "user_handle"=>"@coke_snortr"},
+            {"_id"=>16, "count"=>1, "created_at"=>"2012-12-07T16:55:45Z", "followers"=>3, "id"=>16, "message"=>"Hi there", "sentiment"=>"0.0", "updated_at"=>"2012-12-07T16:55:45Z", "user_handle"=>"@idiot"}
+          ])
+          response.status.should eq(200)
+        end
+      end
+    end
   end
 
 end
