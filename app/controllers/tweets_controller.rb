f1f7c016f533c6a677095a10d5b8dc541b0892cf
Carlos Vilhena <carlosvilhena@gmail.com>
2012-12-09 18:09:05 +0000

    order list by sentiment, desc

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
   respond_to :json, :html
 
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.desc(:sentiment)
     respond_with @tweets
   end
 end
