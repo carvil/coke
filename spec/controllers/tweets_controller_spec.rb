d09a78d5dc5c6510abbf437e1930c78b70eeac58
Carlos Vilhena <carlosvilhena@gmail.com>
2012-12-09 20:18:23 +0000

    test the user route

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -24,4 +24,16 @@ describe TweetsController do
     end
   end
 
+  describe "GET 'user'" do
+    context "given a user handle" do
+      it "should only return messages specific to that user" do
+        VCR.use_cassette('success_controller_testing', :record => :new_episodes) do
+          Tweet.load
+          get 'user', format: :json, user_handle: "@idiot"
+          JSON.parse(response.body).should eq([{"_id"=>16, "count"=>1, "created_at"=>"2012-12-07T16:55:45Z", "followers"=>3, "id"=>16, "message"=>"Hi there", "sentiment"=>0.0, "updated_at"=>"2012-12-07T16:55:45Z", "user_handle"=>"@idiot"}])
+        end
+      end
+    end
+  end
+
 end
