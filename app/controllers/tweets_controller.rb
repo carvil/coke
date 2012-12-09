9de72c76b3cbda6a6687a98fbdce10d3070a3b86
Carlos Vilhena <carlosvilhena@gmail.com>
2012-12-09 18:46:52 +0000

    add link to load tweets via ujs

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,9 +1,22 @@
 class TweetsController < ApplicationController
 
-  respond_to :json, :html
+  respond_to :json, :html, :js
 
   def index
-    @tweets = Tweet.desc(:sentiment)
+    @tweets = tweets_desc_sentiment
     respond_with @tweets
+  end
+
+  def load
+    # Need to check for errors and show message is anything is wrong
+    Tweet.load
+    @tweets = tweets_desc_sentiment
+    respond_with @tweets
+  end
+
+  private
+
+  def tweets_desc_sentiment
+    Tweet.desc(:sentiment)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Coke::Application.routes.draw do
   resources :tweets, only: [:index]
+  get :load, controller: :tweets, action: :load
 
   root :to => 'tweets#index'
 end
